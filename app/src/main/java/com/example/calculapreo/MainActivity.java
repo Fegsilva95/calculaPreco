4f41c946f263ee0a357b61624d712a372c2016dc
Fernanda <12787529607@ice.ufjf.br>
2022-05-25 22:02:25 -0300

    add function to calculate product value

--- app/src/main/java/com/example/calculapreo/MainActivity.java
+++ app/src/main/java/com/example/calculapreo/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.calculapreo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView precoFinal;
+    private EditText precoProduto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        precoProduto = findViewById(R.id.precoProduto);
+        precoFinal = findViewById(R.id.precoFInal);
+    }
+
+    public void calcular(View view){
+        precoFinal.setText(precoProduto.getText());
     }
 }
